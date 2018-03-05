d1932eb4f677d1579d972dc8e69bd2c4759b386e
Michael Beckmann <michael.beckmann@ufz.de>
2018-03-05 12:45:44 +0100

    Major clean-up interim

--- 02_within_SPA_analysis.R
+++ 02_within_SPA_analysis.R
@@ -1,121 +1,8 @@
 ############################################################################
-### 00.1. set the working and temporary directories
-###
-### checks for nodename or username and sets directories accordingly
+### 02.1. overlay of SPAs with ACTs
 ############################################################################
 
-.setwdntemp <- function(){
-  cu <- Sys.info()["user"]
-  cn <- Sys.info()["nodename"]
-  
-  if (cn == "juro-MacBookPro"){
-    path2wd <- "/home/juro/git/FAWKES-III/" #MB
-    path2temp <- "/home/juro/git/FAWKES-III/temp/" #MB
-    
-  } else if (cn == "enter your name here"){
-    path2wd <- "/Users/..."
-    path2temp <- "/Users/...tmp/" 
-  }  
-  return(list(path2temp,path2wd))
-}
-
-set.list <-  .setwdntemp()
-path2temp <- set.list[[1]]
-path2wd <- set.list[[2]]
-
-############################################################################
-### 1. load libraries and functions
-############################################################################
-
-needed_libs <- c("ggplot2",# For plotting
-                 "raster",# for adding map data
-                 "rgdal", # for loading map data
-                 "rgeos", # dependency for rgdal
-                 "letsR",
-                 "foreign",
-                 #"ggplot",
-                 "MASS",
-                 "Hmisc",
-                 "reshape2",
-                 "VGAM"
-)
-
-usePackage <- function(p) {
-  if (!is.element(p, installed.packages()[,1]))
-    install.packages(p, dep = TRUE)
-  require(p, character.only = TRUE)
-}
-sapply(needed_libs,usePackage)
-
-rm(needed_libs)
-
-
-############################################################################
-### 2. load Natura2000 Species data
-############################################################################
-
-setwd(path2temp)
-
-if (file.exists("PublicNatura2000End2016_csv.zip")==FALSE){
-  download.file("https://www.dropbox.com/s/52y55t4qdjaflhi/PublicNatura2000End2016_csv.zip?dl=1", "PublicNatura2000End2016_csv.zip", mode="wb")
-  unzip("PublicNatura2000End2016_csv.zip")
-} else {unzip("PublicNatura2000End2016_csv.zip")}
-
-### create table of all Bird species with Sitecodes and Conservation Status
-
-N2000Species <- read.csv("SPECIES.csv",header=TRUE)
-mySpeciesdata <- N2000Species[which(N2000Species$SPGROUP=="Birds"),c(2,3,4,16)] # only bird species
-mySpeciesdata <- mySpeciesdata[which(mySpeciesdata$CONSERVATION=="A"|mySpeciesdata$CONSERVATION=="B"|mySpeciesdata$CONSERVATION=="C"),]       # remove Conservation Status = "NULL"
-
-N2000Sites <- read.csv("NATURA2000SITES.csv",header=TRUE) 
-mySPAsites <- N2000Sites[which(N2000Sites$SITETYPE=="A"|N2000Sites$SITETYPE=="C"),c(2,3)]  # take only SPA sites (where SITETYPE is A or C) and columns 2 and 3
-
-mydata <- merge(mySpeciesdata, mySPAsites, by="SITECODE")
-
-### load bird species lost and categories
-
-if (file.exists("bird_categorization.csv")==FALSE){
-  download.file("https://www.dropbox.com/s/bxf39frp982fe4z/bird%20categorization.csv?dl=1", "bird_categorization.csv", mode="wb")
-  bird<-read.csv("bird_categorization.csv",sep=";")
-} else {bird<-read.csv("bird_categorization.csv",sep=";")}
-
-
-############################################################################
-### 3. Load archetypes raster and Natura2000 shapefiles
-############################################################################
-
-if (file.exists("Archetypes_Levers_et_al_2015.zip")==FALSE){
-  download.file("https://www.dropbox.com/s/2qybuvj1balvcwo/Archetypes_Levers_et_al_2015.zip?dl=1", "Archetypes_Levers_et_al_2015.zip", mode="wb")
-  unzip("Archetypes_Levers_et_al_2015.zip")
-} else {unzip("Archetypes_Levers_et_al_2015.zip")}
-
-# load land system archetypes (15 classes)
-LSA<-raster("LandSystemArchetypes_2006_Levers2015.tif")
-#plot(LSA)
-#hist(LSA)
-
-# load archetypes change trajectories (17 classes)
-ACT<-raster("ArchetypicalChangeTrajectories_1990_2006_Levers2015.tif")
-values(ACT)[values(ACT) > 17] = NA       # there are only 17 categories: put all values > 17 to NAs
 #plot(ACT)
-#hist(ACT)
-
-# load N2000 shapefile
-Natura2000_shape <- readOGR(dsn = ".", layer = "Natura2000_end2016") 
-sitecodes <- Natura2000_shape$SITECODE 
-
-# subset N2000 dataset to use only SPAs and "both" (i.e. only those sites that are focusing on bird conservation)
-N2000SPASiteCodes <- subset(N2000Sites$SITECODE,N2000Sites$SITETYPE=="A"|N2000Sites$SITETYPE=="C")  # 5572 sitecodes
-
-# There are 3 Sitecodes which cannot be found in the SPA Shapefiles
-overlapSPA <- sitecodes[which(as.character(sitecodes)%in%as.character(N2000SPASiteCodes)==T)]       # 5569 sitecodes
-length(N2000SPASiteCodes)-length(overlapSPA)
-
-############################################################################
-### 4. overlay of SPAs with ACTs
-############################################################################
-
-plot(ACT)
 Natura2000_shape_all <- Natura2000_shape[which(as.character(sitecodes)%in%as.character(N2000SPASiteCodes)==T),]
 
 # extract the ACTs, LSAs and area of the SPAs sitecode-wise
@@ -146,93 +33,6 @@ for (i in 1:total){
 close(pb)
 
 #ACT_ext[[1]]
-
-###############################################################################
-### Buffer stuffer
-###############################################################################
-
-# subset shapefile of 10 SPAs
-subset<-Natura2000_shape_all[1:10,]
-#plot(subset, col="red", add=TRUE)    # only subset for now
-
-#fun with outer buffers
-outerBuffer<-function(x, d){
-  buff<-buffer(x, width = d, dissolve = F)
-  e<-erase(buff,x)
-  return(e)
-}
-
-buffer10<-list()
-# outer10km<-outerBuffer(x=subset,d=10000)
-
-myBuffer<-list()
-for(i in 1:length(subset)){
-  myBuffer[[i]]<-outerBuffer(x=subset[i,],d=10000)
-}
-
-plot(ACT)
-plot(myBuffer[[1]], col="red", add=TRUE)
-
-pb <- txtProgressBar(min = 0, max = total, style = 3) # produces a progress bar
-for (i in 1:length(myBuffer)){
-  subset10 <- myBuffer[[i]]
-  # takes each shapefile, if it is a SPA
-  buffer10[[i]]<-extract(ACT,subset10,weights=TRUE,normalizeWeights=TRUE)
-  area[[i]]<-gArea(subset10)/1e+06
-  perimeter[[i]]<-gLength(subset10)/1e+03
-  setTxtProgressBar(pb, i)
-}
-close(pb)
-
-#cleanup
-
-ACT_red<-list()
-area_red<-list()
-
-for (i in 1:length(buffer10)){
-  if(is.null(buffer10[[i]][[1]])==TRUE) {
-    ACT_red[[i]]<-NA  
-    area_red[[i]]<-NA
-  }else{
-    if(isTRUE(unique(is.na(as.data.frame(buffer10[[i]])$value)))==TRUE){
-      ACT_red[[i]]<-NA  
-      area_red[[i]]<-NA
-    }else{
-      f.correct<-1/sum(as.data.frame(buffer10[[i]])[!is.na(as.data.frame(buffer10[[i]])[,1]),2])
-      reduced.na<-as.data.frame(buffer10[[i]])[!is.na(as.data.frame(buffer10[[i]])[,1]),]
-      reduced.na[,2]<-reduced.na[,2]*f.correct
-      ACT_red[[i]]<-reduced.na
-      area_red[[i]]<-area[[i]]*sum(as.data.frame(buffer10[[i]])[!is.na(as.data.frame(buffer10[[i]])[,1]),2])
-    }
-  }    
-}   
-
-
-
-# create a dataframe for presenting the distribution of SPA-area per ACT
-results <- as.data.frame(matrix(NA,length(ACT_red),20))
-colnames(results) <- c("SITECODE","area",paste("ACT",c(1:17),sep=""),"count")
-
-for(k in 1:length(ACT_red)){
-  results[k,1]<-as.character(overlapSPA)[k]
-  results[k,2]<-area_red[[k]]
-  
-  if(is.na(ACT_red[[k]])==TRUE){
-    results[k,3:19]<-NA
-  } else{
-    test.sum<-cbind(aggregate(weight~value,sum,data=ACT_red[[k]]),table(ACT_red[[k]]$value))
-    results[k,test.sum$value+2]<-test.sum$weight
-    results[k,20]<-sum(test.sum$Freq)
-    
-  }
-  print(k)
-}
-
-
-
-results[is.na(results)]<-0
-results2 <- results[which(results$count>5),]
-
 
 
 ###############################################################################
@@ -328,35 +128,6 @@ which(table(bird$SPECIESCODE)>1)
 #load("FAWKES.RData")
 
 
-
-
-
-############################################################################
-### 
-############################################################################
-
-
-for (i in 1:length(dat$SPECIESCODE)){
-
-  dat$ACT_dom[i]<-colnames(dat[i,8:11])[max.col(dat[i,8:11])]
-
-  }
-
-
-bird2<-reshape(aggregate (dat$ACT_dom, list(dat$SPECIESCODE,dat$ACT_dom), FUN=length), v.names="x", timevar="Group.2", idvar="Group.1", direction="wide")
-colnames(bird2)<-c("SPECIESCODE","Conversion", "De_intens" ,"Intens" ,"Stabil")
-bird<-merge(bird,bird2, by="SPECIESCODE")
-
-colnames(bird)<-c("SPECIESCODE", "migration"   ,"preference","Conversion", "De_intens" ,"Intens" ,"Stabil")
-
-for (i in 1:length(bird$SPECIESCODE)){
-  
-  bird$ACT_dom_dom[i]<-colnames(bird[i,4:7])[max.col(bird[i,4:7])]
-  
-}
-
-
-
 ############################################################################
 ### 6. correlations between Conservation status and ACTs
 ############################################################################
@@ -383,6 +154,33 @@ par(mfrow=c(3,1))
 boxplot(subset(dat,dat$CONSERVATION=="A")[,c(8:11)],col="green")
 boxplot(subset(dat,dat$CONSERVATION=="B")[,c(8:11)],col="yellow")
 boxplot(subset(dat,dat$CONSERVATION=="C")[,c(8:11)],col="red")
+
+
+############################################################################
+### 
+############################################################################
+
+
+for (i in 1:length(dat$SPECIESCODE)){
+  
+  dat$ACT_dom[i]<-colnames(dat[i,8:11])[max.col(dat[i,8:11])]
+  
+}
+
+
+bird2<-reshape(aggregate (dat$ACT_dom, list(dat$SPECIESCODE,dat$ACT_dom), FUN=length), v.names="x", timevar="Group.2", idvar="Group.1", direction="wide")
+colnames(bird2)<-c("SPECIESCODE","Conversion", "De_intens" ,"Intens" ,"Stabil")
+bird<-merge(bird,bird2, by="SPECIESCODE")
+
+colnames(bird)<-c("SPECIESCODE", "migration"   ,"preference","Conversion", "De_intens" ,"Intens" ,"Stabil")
+
+for (i in 1:length(bird$SPECIESCODE)){
+  
+  bird$ACT_dom_dom[i]<-colnames(bird[i,4:7])[max.col(bird[i,4:7])]
+  
+}
+
+
 
 
 ##################### models for subgroup of birds #######################
--- 03_buffer_analysis.R
+++ 03_buffer_analysis.R
@@ -1,117 +1,4 @@
 ############################################################################
-### 00.1. set the working and temporary directories
-###
-### checks for nodename or username and sets directories accordingly
-############################################################################
-
-.setwdntemp <- function(){
-  cu <- Sys.info()["user"]
-  cn <- Sys.info()["nodename"]
-  
-  if (cn == "juro-MacBookPro"){
-    path2wd <- "/home/juro/git/FAWKES-III/" #MB
-    path2temp <- "/home/juro/git/FAWKES-III/temp/" #MB
-    
-  } else if (cn == "enter your name here"){
-    path2wd <- "/Users/..."
-    path2temp <- "/Users/...tmp/" 
-  }  
-  return(list(path2temp,path2wd))
-}
-
-set.list <-  .setwdntemp()
-path2temp <- set.list[[1]]
-path2wd <- set.list[[2]]
-
-############################################################################
-### 1. load libraries and functions
-############################################################################
-
-needed_libs <- c("ggplot2",# For plotting
-                 "raster",# for adding map data
-                 "rgdal", # for loading map data
-                 "rgeos", # dependency for rgdal
-                 "letsR",
-                 "foreign",
-                 #"ggplot",
-                 "MASS",
-                 "Hmisc",
-                 "reshape2",
-                 "VGAM"
-)
-
-usePackage <- function(p) {
-  if (!is.element(p, installed.packages()[,1]))
-    install.packages(p, dep = TRUE)
-  require(p, character.only = TRUE)
-}
-sapply(needed_libs,usePackage)
-
-rm(needed_libs)
-
-
-############################################################################
-### 2. load Natura2000 Species data
-############################################################################
-
-setwd(path2temp)
-
-if (file.exists("PublicNatura2000End2016_csv.zip")==FALSE){
-  download.file("https://www.dropbox.com/s/52y55t4qdjaflhi/PublicNatura2000End2016_csv.zip?dl=1", "PublicNatura2000End2016_csv.zip", mode="wb")
-  unzip("PublicNatura2000End2016_csv.zip")
-} else {unzip("PublicNatura2000End2016_csv.zip")}
-
-### create table of all Bird species with Sitecodes and Conservation Status
-
-N2000Species <- read.csv("SPECIES.csv",header=TRUE)
-mySpeciesdata <- N2000Species[which(N2000Species$SPGROUP=="Birds"),c(2,3,4,16)] # only bird species
-mySpeciesdata <- mySpeciesdata[which(mySpeciesdata$CONSERVATION=="A"|mySpeciesdata$CONSERVATION=="B"|mySpeciesdata$CONSERVATION=="C"),]       # remove Conservation Status = "NULL"
-
-N2000Sites <- read.csv("NATURA2000SITES.csv",header=TRUE) 
-mySPAsites <- N2000Sites[which(N2000Sites$SITETYPE=="A"|N2000Sites$SITETYPE=="C"),c(2,3)]  # take only SPA sites (where SITETYPE is A or C) and columns 2 and 3
-
-mydata <- merge(mySpeciesdata, mySPAsites, by="SITECODE")
-
-### load bird species lost and categories
-
-if (file.exists("bird_categorization.csv")==FALSE){
-  download.file("https://www.dropbox.com/s/bxf39frp982fe4z/bird%20categorization.csv?dl=1", "bird_categorization.csv", mode="wb")
-  bird<-read.csv("bird_categorization.csv",sep=";")
-} else {bird<-read.csv("bird_categorization.csv",sep=";")}
-
-
-############################################################################
-### 3. Load archetypes raster and Natura2000 shapefiles
-############################################################################
-
-if (file.exists("Archetypes_Levers_et_al_2015.zip")==FALSE){
-  download.file("https://www.dropbox.com/s/2qybuvj1balvcwo/Archetypes_Levers_et_al_2015.zip?dl=1", "Archetypes_Levers_et_al_2015.zip", mode="wb")
-  unzip("Archetypes_Levers_et_al_2015.zip")
-} else {unzip("Archetypes_Levers_et_al_2015.zip")}
-
-# load land system archetypes (15 classes)
-LSA<-raster("LandSystemArchetypes_2006_Levers2015.tif")
-#plot(LSA)
-#hist(LSA)
-
-# load archetypes change trajectories (17 classes)
-ACT<-raster("ArchetypicalChangeTrajectories_1990_2006_Levers2015.tif")
-values(ACT)[values(ACT) > 17] = NA       # there are only 17 categories: put all values > 17 to NAs
-#plot(ACT)
-#hist(ACT)
-
-# load N2000 shapefile
-Natura2000_shape <- readOGR(dsn = ".", layer = "Natura2000_end2016") 
-sitecodes <- Natura2000_shape$SITECODE 
-
-# subset N2000 dataset to use only SPAs and "both" (i.e. only those sites that are focusing on bird conservation)
-N2000SPASiteCodes <- subset(N2000Sites$SITECODE,N2000Sites$SITETYPE=="A"|N2000Sites$SITETYPE=="C")  # 5572 sitecodes
-
-# There are 3 Sitecodes which cannot be found in the SPA Shapefiles
-overlapSPA <- sitecodes[which(as.character(sitecodes)%in%as.character(N2000SPASiteCodes)==T)]       # 5569 sitecodes
-length(N2000SPASiteCodes)-length(overlapSPA)
-
-############################################################################
 ### 4. overlay of SPAs with ACTs
 ############################################################################
 
@@ -325,7 +212,7 @@ tabFinal <- merge(tabFinal,bird, by="SPECIESCODE")
 which(table(bird$SPECIESCODE)>1)
 
 #save.image(file = "FAWKES.RData")
-#load("FAWKES.RData")
+
 
 
 
@@ -337,10 +224,10 @@ which(table(bird$SPECIESCODE)>1)
 
 
 for (i in 1:length(dat$SPECIESCODE)){
-
+  
   dat$ACT_dom[i]<-colnames(dat[i,8:11])[max.col(dat[i,8:11])]
-
-  }
+  
+}
 
 
 bird2<-reshape(aggregate (dat$ACT_dom, list(dat$SPECIESCODE,dat$ACT_dom), FUN=length), v.names="x", timevar="Group.2", idvar="Group.1", direction="wide")
